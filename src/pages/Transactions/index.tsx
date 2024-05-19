f2f52150665e751f51ed38418f17c28106d832dc
RafaelRossales <rapha2704@gmail.com>
2024-05-19 20:55:59 -0300

    Data formmat

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,13 +2,13 @@ import { useContext } from "react";
 import { Header } from "../../components/Header";
 import Summary from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
-import { TransactionTable, TransactionsContainer } from "./styles";
+import { PriceHighlight, TransactionTable, TransactionsContainer } from "./styles";
 import TransactionContext from "../../contexts/TransactionContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 
 export function Transaction(){
-    const { transactions } = useContext(TransactionContext)
-
+    const { transactions } = useContext(TransactionContext);
     return(
         <div>
             <Header/>
@@ -21,9 +21,14 @@ export function Transaction(){
                         return(
                             <tr key={transaction.id}>
                                 <td width="50%">{transaction.description}</td>
-                                <td>{transaction.price}</td>
-                                <td>{transaction.type}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>
+                                    <PriceHighlight variant={transaction.type}>
+                                        {transaction.type === 'outcome' && '- '}
+                                            {priceFormatter.format(transaction.price)}
+                                    </PriceHighlight>
+                                </td>
+                                <td>{transaction.category}</td>
+                                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                             </tr>
                         )
                     })}
