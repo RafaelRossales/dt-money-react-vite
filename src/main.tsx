a7666d6a52de36c464b7a9714aa0b6202c589cc6
RafaelRossales <rapha2704@gmail.com>
2024-05-19 20:15:28 -0300

    Request API for data transactions using useEffect hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-// import React from 'react'
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client';
 import {App} from './App.tsx'
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+import.meta.env.API_URL
+
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import Summary from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionTable, TransactionsContainer } from "./styles";
+const URL:string = import.meta.env.VITE_API_URL;
+
+interface ITransaction{
+    id:number;
+    description:string;
+    type:'income' | 'outcome';
+    price:number;
+    category:string;
+    createdAt:string
+}
+
 
 export function Transaction(){
+
+    const [transactions,setTransactions ]= useState<ITransaction[]>([]);
+
+
+    async function loadTransaction(){
+        const response = await fetch(`${URL}/transactions`)
+        const data = await response.json()
+        setTransactions(data)
+    }
+
+    useEffect(()=>{
+        loadTransaction()
+    },[])
+
+
     return(
         <div>
             <Header/>
@@ -12,42 +39,16 @@ export function Transaction(){
                 <SearchForm/>
             <TransactionTable>
                 <tbody>
-                    <tr>
-                        <td width="50%"> Desenvolvimento de site</td>
-                        <td>R$ 12.000,00</td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
-                    <tr>
-                        <td width="50%"> Desenvolvimento de site</td>
-                        <td><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
-                    <tr>
-                        <td width="50%"> Hamburguer</td>
-                        <td>R$ 12.000,00</td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
-                    <tr>
-                        <td width="50%"> Desenvolvimento de site</td>
-                        <td><PriceHighlight variant="outcome">R$ 12.000,00</PriceHighlight></td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
-                    <tr>
-                        <td width="50%"> Desenvolvimento de site</td>
-                        <td>R$ 12.000,00</td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
-                    <tr>
-                        <td width="50%"> Desenvolvimento de site</td>
-                        <td>R$ 12.000,00</td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
+                    {transactions.map(transaction =>{
+                        return(
+                            <tr key={transaction.id}>
+                                <td width="50%">{transaction.description}</td>
+                                <td>{transaction.price}</td>
+                                <td>{transaction.type}</td>
+                                <td>{transaction.createdAt}</td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </TransactionTable>
             </TransactionsContainer>
